371909442e607f50a05c653d116d4d37cf8dd2e0
Yuki Ono <ykon0x1@gmail.com>
2020-07-29 01:54:45 +0900

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,338 @@
+/**
+ * Copyright (c) Yuki Ono.
+ * Licensed under the MIT License.
+ */
+
+package app
+
+import com.beust.klaxon.Klaxon
+import java.io.*
+import java.math.BigDecimal
+import java.net.InetAddress
+import java.net.Socket
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+val VERSION = BigDecimal("1.0")
+const val CRLF = "\r\n"
+
+fun findLfIndex(fileBuf: ByteArray, offset: Int): Int {
+    var i = offset
+    while (i < fileBuf.size) {
+        if (fileBuf[i] == '\n'.toByte())
+            return i
+        i += 1
+    }
+    return -1
+}
+
+fun findAllLfIndices(fileBuf: ByteArray): List<Int> {
+    val indices = mutableListOf<Int>()
+    var i = 0
+    while (true) {
+        i = findLfIndex(fileBuf, i)
+        if (i == -1)
+            return indices
+
+        indices.add(i)
+        i += 1
+    }
+}
+
+fun copyNew(srcBuf: ByteArray, offset: Int, endIndex: Int): ByteArray {
+    val destBuf = ByteArray(endIndex - offset)
+    srcBuf.copyInto(destBuf, 0, offset, endIndex)
+    return destBuf
+}
+
+fun getRawLines(fileBuf: ByteArray): List<ByteArray> {
+    var offset = 0
+    return findAllLfIndices(fileBuf).plus(fileBuf.size - 1).map {
+        val line = copyNew(fileBuf, offset, it + 1)
+        offset = it + 1
+        return@map line
+    }
+}
+
+val DATE_REGEX = Regex("""^Date: [\S ]+""")
+
+fun isDateLine(line: String): Boolean {
+    return DATE_REGEX.containsMatchIn(line)
+}
+
+fun makeNowDateLine(): String {
+    val time = OffsetDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss Z", Locale.US)
+    return "Date: " + time.format(formatter)
+}
+
+val MESSAGE_ID_REGEX = Regex("""^Message-ID: [\S]+""")
+
+fun isMessageIdLine(line: String): Boolean {
+    return MESSAGE_ID_REGEX.containsMatchIn(line)
+}
+
+fun makeRandomMessageIdLine(length: Int = 62): String {
+    val chars =  ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    return "Message-ID: <${(1..length).map { chars.random() }.joinToString("")}>"
+}
+
+fun isFirstD(line: ByteArray): Boolean {
+    return line.firstOrNull() == 'D'.toByte()
+}
+
+fun isFirstM(line: ByteArray): Boolean {
+    return line.firstOrNull() == 'M'.toByte()
+}
+
+fun findDateLineIndex(lines: List<ByteArray>): Int {
+    return lines.indexOfFirst { isFirstD(it) && isDateLine(it.toString(Charsets.UTF_8)) }
+}
+
+fun findMessageIdLineIndex(lines: List<ByteArray>): Int {
+    return lines.indexOfFirst { isFirstM(it) && isMessageIdLine(it.toString(Charsets.UTF_8)) }
+}
+
+fun replaceRawLines(lines: List<ByteArray>, updateDate: Boolean, updateMessageId: Boolean): List<ByteArray> {
+    if (!updateDate && !updateMessageId)
+        return lines
+
+    val repsLines = lines.toMutableList()
+
+    if (updateDate) {
+        val dateIdx = findDateLineIndex(repsLines)
+        if (dateIdx != -1)
+            repsLines[dateIdx] = makeNowDateLine().toByteArray(Charsets.UTF_8)
+    }
+    if (updateMessageId) {
+        val midIdx = findMessageIdLineIndex(repsLines)
+        if (midIdx != -1)
+            repsLines[midIdx] = makeRandomMessageIdLine().toByteArray(Charsets.UTF_8)
+    }
+
+    return repsLines
+}
+
+fun concatRawLines(lines: List<ByteArray>): ByteArray {
+    val buf = ByteArray(lines.sumBy { it.size })
+    var offset = 0
+    for (l in lines) {
+        l.copyInto(buf, offset, 0, l.size)
+        offset += l.size
+    }
+    return buf
+}
+
+fun replaceRawBytes(fileBuf: ByteArray, updateDate: Boolean, updateMessageId: Boolean): ByteArray {
+    return concatRawLines(replaceRawLines(getRawLines(fileBuf), updateDate, updateMessageId))
+}
+
+@Volatile var useParallel = false
+
+fun getCurrentIdPrefix(): String {
+    return if (useParallel) "id: ${Thread.currentThread().id}, " else ""
+}
+
+fun sendRawBytes(output: OutputStream, file: String, updateDate: Boolean, updateMessageId: Boolean): Unit {
+    val fileObj = File(file)
+    println(getCurrentIdPrefix() + "send: ${fileObj.absolutePath}")
+
+    val buf = replaceRawBytes(fileObj.readBytes(), updateDate, updateMessageId)
+    output.write(buf)
+    output.flush()
+}
+
+fun ByteArray.toHexString() = joinToString("") { "%02x".format(it) }
+
+data class Settings (
+    val smtpHost: String,
+    val smtpPort: Int,
+    val fromAddress: String,
+    val toAddress: List<String>,
+    val emlFile: List<String>,
+    val updateDate: Boolean,
+    val updateMessageId: Boolean,
+    val useParallel: Boolean
+)
+
+fun getSettings(file: String): Settings? {
+    return Klaxon().parse<Settings>(File(file).readText())
+}
+
+val RECV_REGEX = Regex("""^\d{3} .+""")
+
+fun isSuccess(line: String): Boolean {
+    return arrayOf('2', '3').contains(line.firstOrNull() ?: '0')
+}
+
+fun recvLine(reader: BufferedReader): String {
+    while (true) {
+        val line = reader.readLine()?.trim() ?: throw IOException("Connection closed by foreign host.")
+        println(getCurrentIdPrefix() + "recv: $line")
+
+        if (RECV_REGEX.containsMatchIn(line)) {
+            if (!isSuccess(line))
+                throw IOException(line)
+
+            return line
+        }
+    }
+}
+
+fun sendLine(writer: BufferedWriter, cmd: String): Unit {
+    println(getCurrentIdPrefix() + "send: " + if (cmd == "$CRLF.") "<CRLF>." else cmd)
+
+    writer.write(cmd)
+    writer.write(CRLF)
+    writer.flush()
+}
+
+typealias SendCmd = (String) -> String
+
+fun makeSendCmd(reader: BufferedReader, writer: BufferedWriter): SendCmd {
+    return { cmd ->
+        sendLine(writer, cmd)
+        recvLine(reader)
+    }
+}
+
+fun sendHello(send: SendCmd): Unit {
+    send("EHLO localhost")
+}
+
+fun sendFrom(send: SendCmd, fromAddr: String): Unit {
+    send("MAIL FROM: <$fromAddr>")
+}
+
+fun sendRcptTo(send: SendCmd, toAddrs: List<String>): Unit {
+    for (addr in toAddrs)
+        send("RCPT TO: <$addr>")
+}
+
+fun sendData(send: SendCmd): Unit {
+    send("DATA")
+}
+
+fun sendCrLfDot(send: SendCmd): Unit {
+    send("$CRLF.")
+}
+
+fun sendQuit(send: SendCmd): Unit {
+    send("QUIT")
+}
+
+fun sendRset(send: SendCmd): Unit {
+    send("RSET")
+}
+
+fun sendMessages(settings: Settings, emlFiles: List<String>): Unit {
+    val addr = InetAddress.getByName(settings.smtpHost)
+    Socket(addr, settings.smtpPort).use { socket ->
+        socket.soTimeout = 1000
+
+        val input = socket.getInputStream()
+        val output = socket.getOutputStream()
+
+        val bufWriter = BufferedWriter(OutputStreamWriter(output))
+        val bufReader = BufferedReader(InputStreamReader(input))
+
+        val send = makeSendCmd(bufReader, bufWriter)
+
+        recvLine(bufReader)
+        sendHello(send)
+
+        var mailSent = false
+        for (file in emlFiles) {
+            if (!File(file).exists()) {
+                println("$file: EML file does not exists")
+                continue
+            }
+
+            if (mailSent) {
+                println("---")
+                sendRset(send)
+            }
+
+            sendFrom(send, settings.fromAddress)
+            sendRcptTo(send, settings.toAddress)
+            sendData(send)
+            sendRawBytes(output, file, settings.updateDate, settings.updateMessageId)
+            sendCrLfDot(send)
+            mailSent = true
+        }
+
+        sendQuit(send)
+    }
+}
+
+fun sendOneMessage(settings: Settings, file: String) {
+    sendMessages(settings, listOf(file))
+}
+
+fun makeJsonSample(): String {
+    return """{
+    "smtpHost": "172.16.3.151",
+    "smtpPort": 25,
+    "fromAddress": "a001@ah62.example.jp",
+    "toAddress": [
+        "a001@ah62.example.jp",
+        "a002@ah62.example.jp",
+        "a003@ah62.example.jp"
+    ],
+    "emlFile": [
+        "test1.eml",
+        "test2.eml",
+        "test3.eml"
+    ],
+    "updateDate": true,
+    "updateMessageId": true,
+    "useParallel": false
+}"""
+}
+
+fun printUsage(): Unit {
+    println("Usage: {self} json_file ...")
+    println("---")
+    println("json_file sample:")
+    println(makeJsonSample())
+}
+
+fun printVersion() {
+    println("SendEML / Version: $VERSION")
+}
+
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        printUsage()
+        return
+    }
+
+    if (args[0] == "--version") {
+        printVersion()
+        return
+    }
+
+    for (json_file in args) {
+        if (!File(json_file).exists()) {
+            println("$json_file: Json file does not exists")
+            continue
+        }
+
+        try {
+            val settings = getSettings(json_file)
+            if (settings != null) {
+                if (settings.useParallel) {
+                    useParallel = true
+                    settings.emlFile.parallelStream().forEach { sendOneMessage(settings, it) }
+                } else {
+                    sendMessages(settings, settings.emlFile);
+                }
+            } else {
+                println("$json_file: Failed to parse?")
+            }
+        } catch (e: Exception) {
+            println("$json_file: ${e.message}")
+        }
+    }
+}
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,345 @@
+/**
+ * Copyright (c) Yuki Ono.
+ * Licensed under the MIT License.
+ */
+
+import org.junit.jupiter.api.Assertions.*
+import java.io.*
+
+typealias SendCmd = (String) -> String
+
+internal class MainKtTest {
+    fun makeSimpleMail(): String {
+        return """From: a001 <a001@ah62.example.jp>
+Subject: test
+To: a002@ah62.example.jp
+Message-ID: <b0e564a5-4f70-761a-e103-70119d1bcb32@ah62.example.jp>
+Date: Sun, 26 Jul 2020 22:01:37 +0900
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101
+ Thunderbird/78.0.1
+MIME-Version: 1.0
+Content-Type: text/plain; charset=utf-8; format=flowed
+Content-Transfer-Encoding: 7bit
+Content-Language: en-US
+
+test""";
+    }
+
+    fun getMailByteArray(mail: String): ByteArray {
+        return mail.replace("\n", "\r\n").toByteArray(Charsets.UTF_8)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun findLfIndex() {
+        val mail = getMailByteArray(makeSimpleMail())
+        assertEquals(34, app.findLfIndex(mail, 0))
+        assertEquals(49, app.findLfIndex(mail, 35))
+        assertEquals(75, app.findLfIndex(mail, 59))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun findAllLfIndices() {
+        val mail = getMailByteArray(makeSimpleMail())
+        val indices = app.findAllLfIndices(mail)
+
+        assertEquals(34, indices[0])
+        assertEquals(49, indices[1])
+        assertEquals(75, indices[2])
+
+        assertEquals(390, indices[indices.size - 3])
+        assertEquals(415, indices[indices.size - 2])
+        assertEquals(417, indices[indices.size - 1])
+    }
+
+    @org.junit.jupiter.api.Test
+    fun copyNew() {
+        val mail = getMailByteArray(makeSimpleMail())
+        val buf = app.copyNew(mail, 0, 10)
+
+        assertEquals(10, buf.size)
+        assertEquals(mail[0], buf[0])
+        assertEquals(mail[1], buf[1])
+        assertEquals(mail[2], buf[2])
+
+        assertEquals(mail[buf.size - 3], buf[buf.size - 3])
+        assertEquals(mail[buf.size - 2], buf[buf.size - 2])
+        assertEquals(mail[buf.size - 1], buf[buf.size - 1])
+
+        buf[0] = 0
+        assertEquals(0, buf[0])
+        assertNotEquals(0, mail[0])
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getRawLines() {
+        val mail = getMailByteArray(makeSimpleMail())
+        val lines = app.getRawLines(mail)
+
+        assertEquals(13, lines.size)
+        assertEquals("From: a001 <a001@ah62.example.jp>\r\n", lines[0].toString(Charsets.UTF_8))
+        assertEquals("Subject: test\r\n", lines[1].toString(Charsets.UTF_8))
+        assertEquals("To: a002@ah62.example.jp\r\n", lines[2].toString(Charsets.UTF_8))
+
+        assertEquals("Content-Language: en-US\r\n", lines[lines.size - 3].toString(Charsets.UTF_8))
+        assertEquals("\r\n", lines[lines.size - 2].toString(Charsets.UTF_8))
+        assertEquals("test", lines[lines.size - 1].toString(Charsets.UTF_8))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun isDateLine() {
+        assertTrue(app.isDateLine("Date: xxx"))
+        assertFalse(app.isDateLine("xxx: Date"))
+        assertFalse(app.isDateLine("X-Date: xxx"))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun makeNowDateLine() {
+        val line = app.makeNowDateLine()
+        assertTrue(line.startsWith("Date: "))
+        assertTrue(line.length <= 76)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun isMessageIdLine() {
+        assertTrue(app.isMessageIdLine("Message-ID: xxx"))
+        assertFalse(app.isMessageIdLine("xxx: Message-ID"))
+        assertFalse(app.isMessageIdLine("X-Message-ID xxx"))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun makeRandomMessageIdLine() {
+        val line = app.makeRandomMessageIdLine()
+        assertTrue(line.startsWith("Message-ID: "))
+        assertTrue(line.length <= 76)
+    }
+
+    fun makeByteArray(vararg cs: Char): ByteArray {
+        return cs.map { it.toByte() }.toByteArray()
+    }
+
+    @org.junit.jupiter.api.Test
+    fun isFirstD() {
+        assertTrue(app.isFirstD(makeByteArray('D', 'A', 'B')))
+        assertTrue(app.isFirstD(makeByteArray('D', 'B', 'A')))
+        assertFalse(app.isFirstD(makeByteArray('A', 'B', 'D')))
+        assertFalse(app.isFirstD(makeByteArray('B', 'A', 'D')))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun isFirstM() {
+        assertTrue(app.isFirstM(makeByteArray('M', 'A', 'B')))
+        assertTrue(app.isFirstM(makeByteArray('M', 'B', 'A')))
+        assertFalse(app.isFirstM(makeByteArray('A', 'B', 'M')))
+        assertFalse(app.isFirstM(makeByteArray('B', 'A', 'M')))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun findDateLineIndex() {
+        val mail = getMailByteArray(makeSimpleMail())
+        val lines = app.getRawLines(mail)
+        assertEquals(4, app.findDateLineIndex(lines))
+
+        val mutLines = lines.toMutableList()
+        mutLines.removeAt(4)
+        assertEquals(-1, app.findDateLineIndex(mutLines))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun findMessageIdLineIndex() {
+        val mail = getMailByteArray(makeSimpleMail())
+        val lines = app.getRawLines(mail)
+        assertEquals(3, app.findMessageIdLineIndex(lines))
+
+        val mutLines = lines.toMutableList()
+        mutLines.removeAt(3)
+        assertEquals(-1, app.findMessageIdLineIndex(mutLines))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun replaceRawLines() {
+        val mail = getMailByteArray(makeSimpleMail())
+        val lines = app.getRawLines(mail)
+
+        val replLinesNoupdate = app.replaceRawLines(lines, false, false)
+        assertEquals(lines, replLinesNoupdate);
+
+        val replLines = app.replaceRawLines(lines, true, true)
+
+        for (i in (0 until 10).filter { it < 3 || it > 4 })
+            assertEquals(lines[i], replLines[i])
+
+        assertNotEquals(lines[3], replLines[3])
+        assertNotEquals(lines[4], replLines[4])
+
+        assertTrue(lines[3].toString(Charsets.UTF_8).startsWith("Message-ID: "))
+        assertTrue(replLines[3].toString(Charsets.UTF_8).startsWith("Message-ID: "))
+        assertTrue(lines[4].toString(Charsets.UTF_8).startsWith("Date: "))
+        assertTrue(replLines[4].toString(Charsets.UTF_8).startsWith("Date: "))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun concatRawLines() {
+        val mail = getMailByteArray(makeSimpleMail())
+        val lines = app.getRawLines(mail)
+
+        val newMail = app.concatRawLines(lines)
+        assertTrue(mail.contentEquals(newMail))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun replaceRawBytes() {
+        val mail = getMailByteArray(makeSimpleMail())
+        val replMailNoupdate = app.replaceRawBytes(mail, false, false)
+        assertNotEquals(mail, replMailNoupdate)
+        assertTrue(mail.contentEquals(replMailNoupdate))
+
+        val replMail = app.replaceRawBytes(mail, true, true)
+        assertNotEquals(mail, replMail)
+        assertFalse(mail.contentEquals(replMail))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getSettings() {
+        val file = createTempFile()
+        file.writeText(app.makeJsonSample())
+
+        val settings = app.getSettings(file.path)
+        assertEquals("172.16.3.151", settings?.smtpHost)
+        assertEquals(25, settings?.smtpPort)
+        assertEquals("a001@ah62.example.jp", settings?.fromAddress)
+        assertTrue(listOf("a001@ah62.example.jp", "a002@ah62.example.jp", "a003@ah62.example.jp")
+                == settings?.toAddress)
+        assertTrue(listOf("test1.eml", "test2.eml", "test3.eml")
+                == settings?.emlFile)
+        assertEquals(true, settings?.updateDate)
+        assertEquals(true, settings?.updateMessageId)
+        assertEquals(false, settings?.useParallel)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun isSuccess() {
+        assertTrue(app.isSuccess("200 xxx"))
+        assertTrue(app.isSuccess("300 xxx"))
+        assertFalse(app.isSuccess("400 xxx"))
+        assertFalse(app.isSuccess("500 xxx"))
+        assertFalse(app.isSuccess("xxx 200"))
+        assertFalse(app.isSuccess("xxx 300"))
+    }
+
+    fun useSetStdout(block: () -> Unit) {
+        val origOut = System.out
+        try {
+            block()
+        } finally {
+            System.setOut(origOut)
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendRawBytes() {
+        val file = createTempFile()
+        val mail = getMailByteArray(makeSimpleMail())
+        file.writeBytes(mail)
+
+        useSetStdout {
+            val stdOutStream = ByteArrayOutputStream()
+            System.setOut(PrintStream(stdOutStream))
+
+            val fileOutStream = ByteArrayOutputStream()
+            app.sendRawBytes(fileOutStream, file.path, false, false)
+            assertEquals("send: ${file.absolutePath}\r\n", stdOutStream.toString())
+            assertTrue(mail.contentEquals(fileOutStream.toByteArray()))
+        }
+
+        val fileOutStream2 = ByteArrayOutputStream()
+        app.sendRawBytes(fileOutStream2, file.path, true, true)
+        assertFalse(mail.contentEquals(fileOutStream2.toByteArray()))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun recvLine() {
+        useSetStdout {
+            val outStream = ByteArrayOutputStream()
+            System.setOut(PrintStream(outStream))
+
+            var bufReader = BufferedReader(StringReader("250 OK\r\n"))
+            assertEquals("250 OK", app.recvLine(bufReader))
+            assertEquals("recv: 250 OK\r\n", outStream.toString())
+        }
+
+        var bufReader2 = BufferedReader(StringReader(""))
+        org.junit.jupiter.api.assertThrows<IOException> { app.recvLine(bufReader2) }
+
+        var bufReader3 = BufferedReader(StringReader("554 Transaction failed\r\n"))
+        org.junit.jupiter.api.assertThrows<IOException> { app.recvLine(bufReader3) }
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendLine() {
+        fun test(cmd: String, stdout_expected: String, writer_expected: String) {
+            val outStream = ByteArrayOutputStream()
+            System.setOut(PrintStream(outStream))
+
+            val strWriter = StringWriter()
+            val bufWriter = BufferedWriter(strWriter)
+
+            app.sendLine(bufWriter, cmd)
+            assertEquals(stdout_expected, outStream.toString())
+            assertEquals(writer_expected, strWriter.toString())
+        }
+
+        useSetStdout {
+            test("EHLO localhost", "send: EHLO localhost\r\n", "EHLO localhost\r\n")
+            test("\r\n.", "send: <CRLF>.\r\n", "\r\n.\r\n")
+        }
+    }
+
+    fun makeTestSendCmd(expected: String): SendCmd {
+        return { cmd ->
+            assertEquals(expected, cmd)
+            cmd
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendHello() {
+        app.sendHello(makeTestSendCmd("EHLO localhost"))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendFrom() {
+        app.sendFrom(makeTestSendCmd("MAIL FROM: <a001@ah62.example.jp>"), "a001@ah62.example.jp")
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendRcptTo() {
+        var count = 1
+        val test: SendCmd = { cmd ->
+            assertEquals("RCPT TO: <a00$count@ah62.example.jp>", cmd)
+            count += 1
+            cmd
+        }
+
+        app.sendRcptTo(test, listOf("a001@ah62.example.jp", "a002@ah62.example.jp", "a003@ah62.example.jp"))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendData() {
+        app.sendData(makeTestSendCmd("DATA"))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendCrLfDot() {
+        app.sendCrLfDot(makeTestSendCmd("\r\n."))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendQuit() {
+        app.sendQuit(makeTestSendCmd("QUIT"))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun sendRset() {
+        app.sendRset(makeTestSendCmd("RSET"))
+    }
+}
