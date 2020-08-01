d3b43bbb75da41762a7d2cdb252825e957d911d1
Yuki Ono <ykon0x1@gmail.com>
2020-08-01 17:42:30 +0900

    Bump version to 1.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,7 +14,7 @@ import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
-val VERSION = BigDecimal("1.0")
+val VERSION = BigDecimal("1.1")
 const val CRLF = "\r\n"
 
 fun findLfIndex(fileBuf: ByteArray, offset: Int): Int {
